e9d8c06671b0f56d7e043f72a4252dbd93e9be9e
Utkarsh Sengar <utkarsh2012@gmail.com>
2017-04-13 23:13:32 -0700

    add button

--- Podfile
+++ Podfile
@@ -6,7 +6,8 @@ target 'Twitter' do
   use_frameworks!
 
   # Pods for Twitter
-  pod 'AFNetworking'
+  pod 'AFNetworking', '3.0.0'
+  pod 'BDBOAuth1Manager', '2.0.0'
   pod 'MBProgressHUD'
 
 end
---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Utkarsh Sengar on 4/13/17.
+//  Copyright © 2017 Area42. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        print("on button click works")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
